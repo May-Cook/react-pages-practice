43e19b4a741ddaea8a39a6407a2df59e2f23c26e
May Cook <benrcook.email@gmail.com>
2023-12-19 22:59:28 +0000

    Random-number-generator to generates an integer from 1 to 100

--- practice-app/components/random-number-generator.component.jsx
+++ practice-app/components/random-number-generator.component.jsx
@@ -8,7 +8,7 @@ export default function RandomNumberGenerator(){
             <input type="range" id="number-range" name="Range" min="0" max="100"/>
             <label for="number-range">Max Value</label>
             <p>Your number is: {num}</p>
-            <div><button onClick={()=> SetNum(58)}>Generate</button></div>
+            <div><button onClick={()=> SetNum(Math.floor(Math.random() * 100) + 1)}>Generate</button></div> {/* Button sets num to a random integer from 1 to 100 */}
         </div>
     )
 }
