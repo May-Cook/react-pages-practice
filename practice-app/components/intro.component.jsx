ac58334b6c286e9c3b0c0b8f2a24119f64d5b164
May Cook <benrcook.email@gmail.com>
2023-12-19 22:42:45 +0000

    Refactor intro into separate component

---
+++ practice-app/components/intro.component.jsx
@@ -0,0 +1,16 @@
+
+export default function Intro(){
+    return(
+        <>
+        <div className='intro'>
+          <p>This site is hosted on <a href="https://pages.github.com/">Github pages</a> and was made using: </p>
+          <ul className='intro-list'>
+            <li><a href="https://react.dev/">React</a></li>
+            <li><a href="https://vitejs.dev/">Vite</a></li>
+            <li><a href="https://bun.sh/">Bun</a></li>
+          </ul>
+          <p>it was created by <a href= "https://github.com/May-Cook">May Cook</a></p>
+        </div>
+        </>
+    )
+}
--- practice-app/src/App.jsx
+++ practice-app/src/App.jsx
@@ -1,22 +1,13 @@
-import RandomNumberGenerator from '../random-number-generator.component'
+import Intro from '../components/intro.component'
+import RandomNumberGenerator from '../components/random-number-generator.component'
 import './App.css'
 
 function App() {
   return (
     <>
-      <div>
-        <h1 className='heading'>React test site</h1>
-        <div className='intro'>
-          <p>This site is hosted on <a href="https://pages.github.com/">Github pages</a> and was made using: </p>
-          <ul className='intro-list'>
-            <li><a href="https://react.dev/">React</a></li>
-            <li><a href="https://vitejs.dev/">Vite</a></li>
-            <li><a href="https://bun.sh/">Bun</a></li>
-          </ul>
-          <p>it was created by <a href= "https://github.com/May-Cook">May Cook</a></p>
-        </div>
-        <RandomNumberGenerator/>
-      </div>
+      <h1 className='heading'>React test site</h1>
+      <Intro/>
+      <RandomNumberGenerator/>
     </>
   )
 }
