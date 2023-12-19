6f0beb27eeee184a2a7314c182d04c355ccdbb13
May Cook <benrcook.email@gmail.com>
2023-12-19 22:52:12 +0000

    Add faked button functinoality to random-number-generator

--- practice-app/components/random-number-generator.component.jsx
+++ practice-app/components/random-number-generator.component.jsx
@@ -1,12 +1,14 @@
+import { useState } from "react"
 
 export default function RandomNumberGenerator(){
+    const [num, SetNum] = useState("");
     return(
         <div className="random-number-generator">
             <h4>Random Number Generator</h4>
             <input type="range" id="number-range" name="Range" min="0" max="100"/>
             <label for="number-range">Max Value</label>
-            <p>Your number is:</p>
-            <div><button>Generate</button></div>
+            <p>Your number is: {num}</p>
+            <div><button onClick={()=> SetNum(58)}>Generate</button></div>
         </div>
     )
 }
