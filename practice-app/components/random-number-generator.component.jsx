d3da950235d77af10b9f35917b5cc4430d07b01e
May Cook <benrcook.email@gmail.com>
2023-12-19 23:21:10 +0000

    Add working slider to set maximum random number

--- practice-app/components/random-number-generator.component.jsx
+++ practice-app/components/random-number-generator.component.jsx
@@ -2,13 +2,14 @@ import { useState } from "react"
 
 export default function RandomNumberGenerator(){
     const [num, SetNum] = useState("");
+    const [maxNum, SetMax] = useState(10);
     return(
         <div className="random-number-generator">
             <h4>Random Number Generator</h4>
-            <input type="range" id="number-range" name="Range" min="0" max="100"/>
-            <label for="number-range">Max Value</label>
+            <input type="range" id="number-range" name="Range" min="1" max="100" value={maxNum} onChange={(e) => SetMax(e.target.value)}/>
+            <label for="number-range">Max Value: {maxNum}</label>
             <p>Your number is: {num}</p>
-            <div><button onClick={()=> SetNum(Math.floor(Math.random() * 100) + 1)}>Generate</button></div> {/* Button sets num to a random integer from 1 to 100 */}
+            <div><button onClick={()=> SetNum(Math.floor(Math.random() * maxNum) + 1)}>Generate</button></div> {/* Button sets num to a random integer from 1 to 100 */}
         </div>
     )
 }
