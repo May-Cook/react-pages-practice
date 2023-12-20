a6c5d791df771d59427ead99816e95c72b8fd5c1
May Cook <benrcook.email@gmail.com>
2023-12-20 00:07:29 +0000

    Add working min slider

--- practice-app/components/random-number-generator.component.jsx
+++ practice-app/components/random-number-generator.component.jsx
@@ -3,13 +3,20 @@ import { useState } from "react"
 export default function RandomNumberGenerator(){
     const [num, SetNum] = useState("");
     const [maxNum, SetMax] = useState(10);
+    const [minNum, SetMin] = useState(1);
     return(
         <div className="random-number-generator">
             <h4>Random Number Generator</h4>
-            <input type="range" id="number-range" name="Range" min="1" max="100" value={maxNum} onChange={(e) => SetMax(e.target.value)}/>
-            <label for="number-range">Max Value: {maxNum}</label>
+            <div>
+                <input type="range" id="max-num" name="Range" min={minNum} max="100" value={maxNum} onChange={(e) => SetMax(+e.target.value)}/>
+                <label for="max-num">Max Value: {maxNum}</label>
+            </div>
+            <div>
+                <input type="range" id="min-num" name="Range" min="1" max={maxNum} value={minNum} onChange={(e) => SetMin(+e.target.value)}/>
+                <label for="min-num">Min Value: {minNum}</label>
+            </div>
             <p>Your number is: {num}</p>
-            <div><button onClick={()=> SetNum(Math.floor(Math.random() * maxNum) + 1)}>Generate</button></div> {/* Button sets num to a random integer from 1 to 100 */}
+            <div><button onClick={()=> SetNum(Math.floor((Math.random() * ((maxNum+1) - minNum)) + minNum))}>Generate</button></div> {/* Button sets num to a random integer from minNum to maxNum */}
         </div>
     )
 }
